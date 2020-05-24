9c7c6f37d20de1adf3cac11bc7a6287c83400c65
Jeswin Roy <jeswinroy1997@gmail.com>
2020-05-24 13:34:42 +0530

    public pages added

--- mysite/pages/views.py
+++ mysite/pages/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from django.http import HttpResponse
 
-# Create your views here.
+def  home_view(request):
+    return render(request, 'pages/index.html')
+
+def  register_view(request):
+
+    if request.method == 'POST':
+        full_name = request.POST['full_name']
+        return HttpResponse(full_name)
+
+    return render(request, 'pages/register.html')
+
